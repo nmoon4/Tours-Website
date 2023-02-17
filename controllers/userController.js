fd75900c33525d82afaa59447e0eaf221aa5fc8f
Noel Moon <nmoon778@gmail.com>
2023-02-17 15:36:50 -0500

    refactoring routes

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+  });
+};
+
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+  });
+};
