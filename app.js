e77ab6a8e429da59b30e480b256d3095ac13e75c
Noel Moon <nmoon778@gmail.com>
2023-02-17 02:43:47 -0500

    added patch to api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const express = require('express');
-const { dirname } = require('path');
+const fs = require("fs");
+const express = require("express");
+const { dirname } = require("path");
 
 const app = express();
 
@@ -10,9 +10,9 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
-    status: 'success',
+    status: "success",
     results: tours.length,
     data: {
       tours: tours,
@@ -20,27 +20,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
+app.get("/api/v1/tours/:id", (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find((el) => el.id === id);
 
   if (!tour) {
     return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
+      status: "fail",
+      message: "Invalid ID",
     });
   }
 
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
       tour: tour,
     },
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {
+app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -51,7 +51,7 @@ app.post('/api/v1/tours', (req, res) => {
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
-        status: 'success',
+        status: "success",
         data: {
           tour: newTour,
         },
@@ -60,11 +60,23 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
-// app.patch('/api/v1/tours/:id', (req, res) => {
-  
-// }
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<nothing>",
+    },
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
-  console.log('App running');
+  console.log("App running");
 });
