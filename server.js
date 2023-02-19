81c88977157ae74b7349330eb9b7902b35121851
Noel Moon <nmoon778@gmail.com>
2023-02-18 23:10:41 -0500

    added mongodb connection

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB Connected');
+  });
+
+const tourSchema = new mongoose.Schema({
+  
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
